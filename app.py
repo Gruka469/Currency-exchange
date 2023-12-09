b4ae9289e100bed4f6fa5bd110675f4b2c2c7204
Gruka469 <lifetroll6@gmail.com>
2023-12-10 00:14:07 +0200

    first step

--- app.py
+++ app.py
@@ -2,6 +2,22 @@ from tkinter import *
 from tkinter import ttk
 import tkinter as tk
 
-window = Tk()
 
+def input_currency():
+    first_currency = tk.Label(text="Currency")
+    first_currency_entry = tk.Entry(window)
+    second_currency = tk.Label(text="Currency")
+    second_currency_entry = tk.Entry(window)
 
+    first_currency.pack(padx=10, pady=10)
+    first_currency_entry.pack(padx=10, pady=10)
+    second_currency.pack(padx=10, pady=10)
+    second_currency_entry.pack(padx=10, pady=10)
+
+window = tk.Tk()
+window.title("Currency Application")
+
+button = tk.Button(window, text="Click me", command=input_currency)
+button.pack(pady=10)
+
+window.mainloop()
