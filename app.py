0f7455251153a8cec5cb8d5e3d40a0894fd4e35d
Gruka469 <lifetroll6@gmail.com>
2023-12-18 15:49:28 +0200

    made the dropdown work and make requests

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import requests
 from currency_stats import get_currencies
 
 #API request for conversion rates
-def get_conversion_rate(target_currency):
+def get_conversion_rate(base_currency, target_currency):
     api_key = 'a73871bc447e4d4aadcbfe76e8857d08'
     url = 'https://openexchangerates.org/api/latest.json?app_id=a73871bc447e4d4aadcbfe76e8857d08' #By default currency = USD
 
@@ -34,11 +34,13 @@ def input_currency():
     first_currency_entry = tk.Entry(submit_root)
     second_currency_label = tk.Label(submit_root, text="Enter Second Currency:")
     second_currency_entry = tk.Entry(submit_root)
-    submit_button = tk.Button(submit_root, text="Submit", command=lambda: submit(first_currency_entry.get(), second_currency_entry.get())) #passing both of the arguments with "get" method
+    submit_button = tk.Button(submit_root, text="Submit", command=lambda: submit(first_currency_entry.get(), second_currency_entry.get(), selected_option, selected_option1)) #passing both of the arguments with "get" method
     #dropdown menu
     selected_option = tk.StringVar(submit_root)
+    selected_option1 = tk.StringVar(submit_root)
     dropdown = tk.OptionMenu(submit_root, selected_option, *get_currencies())
-    dropdown1 = tk.OptionMenu(submit_root, selected_option, *get_currencies())
+    dropdown1 = tk.OptionMenu(submit_root, selected_option1, *get_currencies())
+    
     #grid system
     first_currency_label.grid(row=0, column=0, padx=10, pady=10) #positioning accordingly
     first_currency_entry.grid(row=0, column=1, padx=10, pady=10) #positioning accordingly
@@ -48,7 +50,12 @@ def input_currency():
     dropdown.grid(row=0, column=3, padx=10, pady=10)
     dropdown1.grid(row=1, column=3, padx=10, pady=10)
 
-def submit(first_currency, second_currency):
+def submit(first_currency, second_currency, selected_option, selected_option1):
+    # If the entry fields are empty, use the dropdown values
+    if not first_currency:
+        first_currency = selected_option.get()
+    if not second_currency:
+        second_currency = selected_option1.get()
     # Print or use the entered currencies as needed
     print("First Currency:", first_currency) #printing the currency inputed from the .entry
     print("Second Currency:", second_currency) #printing the currency inputed from the .entry
@@ -58,13 +65,13 @@ def submit(first_currency, second_currency):
     if conversion_rate is not None:
         print(f"Conversion rate from {first_currency} to {second_currency}: {conversion_rate}")
 
-def dropdown_menu(root):
-    options = get_currencies()
+# def dropdown_menu(root):
+#     options = get_currencies()
 
-    selected_option = tk.StringVar(root)
-    selected_option.set(options[0]) #default selected option
-    dropdown_menu = tk.OptionMenu(root, selected_option, *options)
-    dropdown_menu.grid(row=0, column=3, padx=10, pady=10)
+#     selected_option = tk.StringVar(root)
+#     selected_option.set(options[0]) #default selected option
+#     dropdown_menu = tk.OptionMenu(root, selected_option, *options)
+#     dropdown_menu.grid(row=0, column=3, padx=10, pady=10)
 
 
 # Create the main root
